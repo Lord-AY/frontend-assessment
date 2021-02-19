5d9d8654e52668afc376270a41efdbec7d518176
Lord-AY <ayomidealadesulu@gmail.com>
2021-02-19 14:50:03 +0100

    final: Investment page

---
+++ src/components/investment/Investment-plan.vue
@@ -0,0 +1,111 @@
+<template>
+  <div class="rounded-2xl">
+    <div
+      style="background: #008080"
+      class="pt-6 flex flex-col items-center rounded-t-xl"
+    >
+      <p class="text-white">{{ type }}</p>
+      <p class="text-white text-4xl pb-2">
+        {{ interest }}
+        <span class="text-2xl">% P/A</span>
+      </p>
+      <p class="text-white">{{ days }}</p>
+      <svg
+        viewBox="0 0 500 150"
+        preserveAspectRatio="none"
+        style="height: 100%; width: 100%"
+      >
+        <path
+          d="M-2.03,149.30 C0.23,-4.64 504.74,132.53 502.48,3.26 L503.05,150.29 L-2.03,150.29 Z"
+          style="stroke: none; fill: #fff"
+        ></path>
+      </svg>
+    </div>
+    <div class="bg-white flex flex-col items-center rounded-b-xl pb-6">
+      <p class="my-2 text-gray-400">
+        <span class="mr-2"
+          ><svg
+            class="inline"
+            width="10"
+            height="8"
+            viewBox="0 0 10 8"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M10 1.43589L3.58975 7.84614L0 4.28205L1.46156 2.84616L3.58975 4.94874L8.53849 0L10 1.43589Z"
+              fill="#3BB54A"
+            />
+          </svg> </span
+        >Automatically Renewable
+      </p>
+      <p class="my-2 text-gray-400">
+        <span class="mr-2"
+          ><svg
+            class="inline"
+            width="10"
+            height="8"
+            viewBox="0 0 10 8"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M10 1.43589L3.58975 7.84614L0 4.28205L1.46156 2.84616L3.58975 4.94874L8.53849 0L10 1.43589Z"
+              fill="#3BB54A"
+            />
+          </svg> </span
+        >Automatically Renewal Options
+      </p>
+      <p class="my-2 text-gray-400">
+        <span class="mr-2"
+          ><svg
+            class="inline"
+            width="10"
+            height="8"
+            viewBox="0 0 10 8"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M10 1.43589L3.58975 7.84614L0 4.28205L1.46156 2.84616L3.58975 4.94874L8.53849 0L10 1.43589Z"
+              fill="#3BB54A"
+            />
+          </svg> </span
+        >Cancel Renewal Anytime
+      </p>
+      <p class="my-2 text-gray-400">
+        <span
+          ><svg
+            class="inline"
+            width="10"
+            height="8"
+            viewBox="0 0 10 8"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M10 1.43589L3.58975 7.84614L0 4.28205L1.46156 2.84616L3.58975 4.94874L8.53849 0L10 1.43589Z"
+              fill="#3BB54A"
+            />
+          </svg> </span
+        >Fast Automated Payout
+      </p>
+      <button
+        style="background: rgba(0, 128, 128, 1);"
+        class="my-2 text-white w-3/5 rounded-md text-center tracking-wide px-4 py-3"
+      >
+        Make Investment
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    type: String,
+    interest: Number,
+    days: Number,
+  },
+};
+</script>
---
+++ src/views/Investment.vue
@@ -0,0 +1,140 @@
+<template>
+  <div class="py-5">
+    <div class="grid grid-cols-4 gap-3">
+      <div class="flex flex-col col-span-1">
+        <p class="text-3xl text-gray-700 font-bold">Investment</p>
+        <p class="text-md text-gray-500">
+          Choose from our available investment plans
+        </p>
+      </div>
+      <div class="flex justify-start col-span-1">
+        <svg
+          class="mr-2"
+          width="30"
+          height="30"
+          viewBox="0 0 30 30"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          xmlns:xlink="http://www.w3.org/1999/xlink"
+        >
+          <rect width="30" height="30" fill="url(#pattern0)" />
+          <defs>
+            <pattern
+              id="pattern0"
+              patternContentUnits="objectBoundingBox"
+              width="1"
+              height="1"
+            >
+              <use
+                xlink:href="#image0"
+                transform="translate(-0.3 -0.266667) scale(0.0333333)"
+              />
+            </pattern>
+            <image
+              id="image0"
+              width="44"
+              height="44"
+              xlink:href="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACwAAAAsCAYAAAAehFoBAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAGBSURBVFhH7Zc9TgQxDEY5CeIG3ISTcAPoKLkIN1gaWmoatNK2NLRQQmH27SoSGjlZZxwjWeSTPmk0m9hPXudnziSZJnC0JnC0JnC0/jfw5l3k9lXk6lnk8knk4vFonnnHb4zxyA38+S1yvztCnW9sZixzmNsrF/DDWx/o0swlRo9WA9/s/14NYo2JZdUq4OsXPbHHxLSoG3hkZZe2VLoLmH7TEo30qZ42A7OiPQvManK0dg8zMNuQliDC5KrJDFyt7lbkgwFfInfl2SLGa/H2JldNJmBOJy3wwQHAuHYimoA5UrWgkSanJhMw9wAt6MFBFSanJhNwc3cIAq71sQmYG5cWNNLk1OQHDqqwCzhdSzQXXZBdi665rQVV2LWtpTs4ULOPB7vWv8gMnO7yk+56iVJd4ItSfSIVpfoILRpZaUtli1YDI/rNsxCZe6pnl3IBI1Y021APOGOZ09oNanID/xanE0cq9wCguHFhnnnHb7UTzKqhwH+hCRytCRytZMAiPyzHb2ZllQZ9AAAAAElFTkSuQmCC"
+            />
+          </defs>
+        </svg>
+
+        <p class="text-md text-gray-500">Investment Plans</p>
+      </div>
+      <div class="flex justify-start col-span-1">
+        <svg
+          class="mr-2"
+          width="30"
+          height="30"
+          viewBox="0 0 30 30"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          xmlns:xlink="http://www.w3.org/1999/xlink"
+        >
+          <rect
+            width="30"
+            height="30"
+            fill="url(#pattern0)"
+            fill-opacity="0.2"
+          />
+          <defs>
+            <pattern
+              id="pattern0"
+              patternContentUnits="objectBoundingBox"
+              width="1"
+              height="1"
+            >
+              <use
+                xlink:href="#image0"
+                transform="translate(-0.3 -0.266667) scale(0.0333333)"
+              />
+            </pattern>
+            <image
+              id="image0"
+              width="44"
+              height="44"
+              xlink:href="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACwAAAAsCAYAAAAehFoBAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAGBSURBVFhH7Zc9TgQxDEY5CeIG3ISTcAPoKLkIN1gaWmoatNK2NLRQQmH27SoSGjlZZxwjWeSTPmk0m9hPXudnziSZJnC0JnC0JnC0/jfw5l3k9lXk6lnk8knk4vFonnnHb4zxyA38+S1yvztCnW9sZixzmNsrF/DDWx/o0swlRo9WA9/s/14NYo2JZdUq4OsXPbHHxLSoG3hkZZe2VLoLmH7TEo30qZ42A7OiPQvManK0dg8zMNuQliDC5KrJDFyt7lbkgwFfInfl2SLGa/H2JldNJmBOJy3wwQHAuHYimoA5UrWgkSanJhMw9wAt6MFBFSanJhNwc3cIAq71sQmYG5cWNNLk1OQHDqqwCzhdSzQXXZBdi665rQVV2LWtpTs4ULOPB7vWv8gMnO7yk+56iVJd4ItSfSIVpfoILRpZaUtli1YDI/rNsxCZe6pnl3IBI1Y021APOGOZ09oNanID/xanE0cq9wCguHFhnnnHb7UTzKqhwH+hCRytCRytZMAiPyzHb2ZllQZ9AAAAAElFTkSuQmCC"
+            />
+          </defs>
+        </svg>
+
+        <p class="text-md text-gray-300">Create Investments</p>
+      </div>
+      <div class="flex justify-start col-span-1">
+        <svg
+          class="mr-2"
+          width="30"
+          height="30"
+          viewBox="0 0 30 30"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          xmlns:xlink="http://www.w3.org/1999/xlink"
+        >
+          <rect
+            width="30"
+            height="30"
+            fill="url(#pattern0)"
+            fill-opacity="0.2"
+          />
+          <defs>
+            <pattern
+              id="pattern0"
+              patternContentUnits="objectBoundingBox"
+              width="1"
+              height="1"
+            >
+              <use
+                xlink:href="#image0"
+                transform="translate(-0.3 -0.266667) scale(0.0333333)"
+              />
+            </pattern>
+            <image
+              id="image0"
+              width="44"
+              height="44"
+              xlink:href="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACwAAAAsCAYAAAAehFoBAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAGBSURBVFhH7Zc9TgQxDEY5CeIG3ISTcAPoKLkIN1gaWmoatNK2NLRQQmH27SoSGjlZZxwjWeSTPmk0m9hPXudnziSZJnC0JnC0JnC0/jfw5l3k9lXk6lnk8knk4vFonnnHb4zxyA38+S1yvztCnW9sZixzmNsrF/DDWx/o0swlRo9WA9/s/14NYo2JZdUq4OsXPbHHxLSoG3hkZZe2VLoLmH7TEo30qZ42A7OiPQvManK0dg8zMNuQliDC5KrJDFyt7lbkgwFfInfl2SLGa/H2JldNJmBOJy3wwQHAuHYimoA5UrWgkSanJhMw9wAt6MFBFSanJhNwc3cIAq71sQmYG5cWNNLk1OQHDqqwCzhdSzQXXZBdi665rQVV2LWtpTs4ULOPB7vWv8gMnO7yk+56iVJd4ItSfSIVpfoILRpZaUtli1YDI/rNsxCZe6pnl3IBI1Y021APOGOZ09oNanID/xanE0cq9wCguHFhnnnHb7UTzKqhwH+hCRytCRytZMAiPyzHb2ZllQZ9AAAAAElFTkSuQmCC"
+            />
+          </defs>
+        </svg>
+        <p class="text-md text-gray-500">Confirm Investment</p>
+      </div>
+    </div>
+
+    <div class="grid grid-cols-3 gap-4 my-3">
+      <investment-plan type="Silver" :interest="12" days="30 Days" />
+      <investment-plan type="Gold" :interest="13" days="90 Days" />
+      <investment-plan type="Diamon" :interest="13.5" days="180 Days" />
+      <investment-plan type="Platinum" :interest="15" days="12 Months" />
+      <investment-plan type="Silver" :interest="19.50" days="18 Months" />
+    </div>
+  </div>
+</template>
+
+<script>
+import InvestmentPlan from "../components/investment/Investment-plan.vue";
+// @ is an alias to /src
+export default {
+  name: "Home",
+  components: { InvestmentPlan },
+};
+</script>
